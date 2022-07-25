45f628ae91cc770917263d70b3444817101de437
Roopini7004 <107471321+Roopini7004@users.noreply.github.com>
2022-07-25 18:34:39 +0530

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -206,9 +206,9 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
         network = list()
         hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)], 'prev':[0 for i in range(n_inputs+1)]} for i in range(n_hidden)]        
         network.append(hidden_layer)
-        hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)], 'prev':[0 for i in range(n_inputs+1)]} for i in range(n_hidden)]        
+       #hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)], 'prev':[0 for i in range(n_inputs+1)]} for i in range(n_hidden)]        
         network.append(hidden_layer)
-        output_layer = [{'weights':[random() for i in range(n_hidden + 1)],'prev':[0 for i in range(n_hidden+1)]} for i in range(n_outputs)]
+        #output_layer = [{'weights':[random() for i in range(n_hidden + 1)],'prev':[0 for i in range(n_hidden+1)]} for i in range(n_outputs)]
         network.append(output_layer)
         #print(network)
         return network
